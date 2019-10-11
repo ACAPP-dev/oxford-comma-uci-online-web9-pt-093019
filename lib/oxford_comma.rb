268b6c8ebed9150e945d7da27894f1e0d4464570
Andrew Capp <acapp909@gmail.com>
2019-10-11 22:59:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.size == 1
     return array.join
   elsif array.size == 2
-    
+
     return array.join(" and ")
   elsif array.size > 2
     p array[0..-2].join(", ")
