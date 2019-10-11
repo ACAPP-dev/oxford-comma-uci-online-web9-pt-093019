3f6311c352746f588eb937b8106e2e23b0a93380
Andrew Capp <acapp909@gmail.com>
2019-10-11 22:58:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,19 @@
+require 'pry'
+
 def oxford_comma(array)
   if array.size == 1
     return array.join
   elsif array.size == 2
+    binding.pry
     return array.join(" and ")
   elsif array.size > 2
-    puts "Testing"
+    p array[0..-2].join(", ")
+    binding.pry
   end
 
 
 
 
 end
+
+p oxford_comma([apple, pear])
