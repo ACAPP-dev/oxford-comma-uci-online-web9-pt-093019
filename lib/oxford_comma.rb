bb2ca0519ed428f8d9e1d05405a8cb9071299c47
Andrew Capp <acapp909@gmail.com>
2019-10-11 22:59:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.size == 1
     return array.join
   elsif array.size == 2
-    binding.pry
+    
     return array.join(" and ")
   elsif array.size > 2
     p array[0..-2].join(", ")
@@ -16,4 +16,4 @@ def oxford_comma(array)
 
 end
 
-p oxford_comma(["apple", "pear"])
+p oxford_comma(%w(apple pear banana))
